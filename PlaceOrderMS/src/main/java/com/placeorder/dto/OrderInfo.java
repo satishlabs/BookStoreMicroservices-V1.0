fb5010107113f8e5ec67cab2e0b05738420c8574
Hell <spd61185@gmail.com>
2020-09-28 14:34:10 +0530

    PlaceOrder Microservices

---
+++ PlaceOrderMS/src/main/java/com/placeorder/dto/OrderInfo.java
@@ -0,0 +1,36 @@
+package com.placeorder.dto;
+
+import java.util.List;
+
+import com.placeorder.entity.Order;
+import com.placeorder.entity.OrderItem;
+
+public class OrderInfo {
+	private Order order;
+	private List<OrderItem> itemList;
+	
+	public OrderInfo() {}
+	public OrderInfo(Order order, List<OrderItem> itemList) {
+		this.order = order;
+		this.itemList = itemList;
+	}
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public List<OrderItem> getItemList() {
+		return itemList;
+	}
+	public void setItemList(List<OrderItem> itemList) {
+		this.itemList = itemList;
+	}
+	@Override
+	public String toString() {
+		return order + ", " + itemList;
+	}
+	
+		
+}
